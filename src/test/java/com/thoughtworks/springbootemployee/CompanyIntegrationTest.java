d344ded90e92d3f43356590b6c98b130f5e3b037
Austin <497133212@qq.com>
2020-07-30 19:46:16 +0800

    [Company API][Austin Huang | Hardy] test : add should_when_delete_company_by_id_given_company_id test

--- src/test/java/com/thoughtworks/springbootemployee/CompanyIntegrationTest.java
+++ src/test/java/com/thoughtworks/springbootemployee/CompanyIntegrationTest.java
@@ -16,7 +16,10 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
+import static org.assertj.core.util.Lists.emptyList;
 import static org.hamcrest.Matchers.hasSize;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -43,29 +46,35 @@ public class CompanyIntegrationTest {
     @Test
     void should_return_companies_when_getAllCompanies_given_request() throws Exception {
         //given
-         List<Company> companies = companyRepository.saveAll(getMockCompanies());
+        List<Company> companies = companyRepository.saveAll(getMockCompanies());
         //when
-         mockMvc.perform(get("/companies"))
+        mockMvc.perform(get("/companies"))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$", hasSize(10)))
-                .andExpect(jsonPath("$[0].companyId").isNumber())
-                .andExpect(jsonPath("$[0].companyId").value(companies.get(0).getId()));
+                .andExpect(jsonPath("$[0].id").isNumber())
+                .andExpect(jsonPath("$[0].id").value(companies.get(0).getId()));
         //then
     }
 
     @Test
     void should_return_employees_when_getAllEmployees_given_() throws Exception {
         //given
-        employeeRepository.saveAll(getMockEmployees());
-        List<Company> companies = companyRepository.saveAll(getMockCompanies());
-        Company company = companyRepository.findById(1).orElse(null);
-        List<Employee> employeesRepo = employeeRepository.findAll();
-        List<Company> companieRepo = companyRepository.findAll();
-        List<Employee> employees = company.getEmployees();
+        Company company = new Company(1, "huawei", 100, emptyList());
+        companyRepository.save(company);
+        List<Employee> employees = new ArrayList<>();
+        employees.add(new Employee(1, "lin", 18, "male", 3000.0, 1));
+        employees.add(new Employee(2, "wang", 20, "male", 5000.0, 1));
+        employees.add(new Employee(3, "huang", 8, "female", 9000.0, 1));
+        employeeRepository.saveAll(employees);
         //when
         mockMvc.perform(get("/companies/1/employees"))
                 .andExpect(status().isOk())
-                .andExpect(jsonPath("$",hasSize(10)));
+                .andExpect(jsonPath("$", hasSize(3)))
+                .andExpect(jsonPath("$[0].id").value(employees.get(0).getId()))
+                .andExpect(jsonPath("$[0].name").value(employees.get(0).getName()))
+                .andExpect(jsonPath("$[0].age").value(employees.get(0).getAge()))
+                .andExpect(jsonPath("$[0].gender").value(employees.get(0).getGender()))
+                .andExpect(jsonPath("$[0].salary").value(employees.get(0).getSalary()));
 
         //then
 
@@ -77,7 +86,7 @@ public class CompanyIntegrationTest {
         //given
         mockMvc.perform(get("/companies/1"))
                 .andExpect(status().isOk())
-                .andExpect(jsonPath("$.companyId").value(companies.get(0).getId()))
+                .andExpect(jsonPath("$.id").value(companies.get(0).getId()))
                 .andExpect(jsonPath("$.employeesNumber").value(companies.get(0).getEmployeesNumber()))
                 .andExpect(jsonPath("$.companyName").value(companies.get(0).getCompanyName()));
         //when
@@ -89,19 +98,19 @@ public class CompanyIntegrationTest {
     @Test
     void should_return_company_when_add_company_given_company() throws Exception {
         //given
-        Company company = new Company(1, "alibaba2", 100 , Collections.emptyList());
+        Company company = new Company(1, "alibaba2", 100, emptyList());
         companyRepository.save(company);
         //when
         mockMvc.perform(post("/companies")
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(
-                      "{\n" +
-                              "        \"id\": 1,\n" +
-                              "        \"companyName\": \"alibaba2\",\n" +
-                              "        \"employeesNumber\": 100,\n" +
-                              "        \"employees\": [\n" +
-                              "        ]\n" +
-                              "    }"
+                        "{\n" +
+                                "        \"id\": 1,\n" +
+                                "        \"companyName\": \"alibaba2\",\n" +
+                                "        \"employeesNumber\": 100,\n" +
+                                "        \"employees\": [\n" +
+                                "        ]\n" +
+                                "    }"
                 ))
                 .andExpect(status().isCreated())
                 .andExpect(jsonPath("$.id").value(company.getId()))
@@ -114,37 +123,45 @@ public class CompanyIntegrationTest {
     @Test
     void should_return_company_when_update_company_given_company() throws Exception {
         //given
-        Company company = new Company(1, "alibaba2", 100 , Collections.emptyList());
+        Company company = new Company(1, "alibaba2", 100, Collections.emptyList());
         companyRepository.save(company);
         //when
         mockMvc.perform(put("/companies/1").contentType(MediaType.APPLICATION_JSON)
                 .content("{\n" +
                         "        \"id\": 1,\n" +
-                        "        \"companyName\": \"alibaba2\",\n" +
+                        "        \"companyName\": \"alibaba3\",\n" +
                         "        \"employeesNumber\": 200,\n" +
                         "        \"employees\": [\n" +
                         "        ]\n" +
                         "    }"))
-                .andExpect(jsonPath("$.employeesNumber").value(companyRepository.findById(1).get().getEmployeesNumber()));
+                .andExpect(jsonPath("$.employeesNumber").value(companyRepository.findById(1).get().getEmployeesNumber()))
+                .andExpect(jsonPath("$.companyName").value(companyRepository.findById(1).get().getCompanyName()));
         //then
     }
 
-
-
-
+    @Test
+    void should_when_delete_company_by_id_given_company_id() throws Exception {
+        //given
+        Company company = getMockCompany();
+        companyRepository.save(company);
+        //when
+        mockMvc.perform(delete("/companies/1")).andExpect(status().isOk());
+        //then
+        assertEquals(false, companyRepository.findById(1).isPresent());
+    }
 
     private List<Company> getMockCompanies() {
         List<Employee> employees = new ArrayList<>();
-        employees.add(new Employee(1, "xiaoyi", 18, "Male", 30000.0));
-        employees.add(new Employee(2, "xiaoer", 18, "Male", 30000.0));
-        employees.add(new Employee(3, "xiaosan", 19, "Male", 30000.0));
-        employees.add(new Employee(4, "xiaosi", 19, "Male", 30000.0));
-        employees.add(new Employee(5, "xiaowu", 20, "Male", 30000.0));
-        employees.add(new Employee(6, "xiaoliu", 20, "Female",30000.0));
-        employees.add(new Employee(7, "xiaoqi", 21, "Female", 30000.0));
-        employees.add(new Employee(8, "xiaoba", 21, "Female", 30000.0));
-        employees.add(new Employee(9, "xiaojiu", 18, "Male", 30000.0));
-        employees.add(new Employee(10, "xiaoshi", 18, "Male", 30000.0));
+        employees.add(new Employee(1, "xiaoyi", 18, "Male", 30000.0,1));
+        employees.add(new Employee(2, "xiaoer", 18, "Male", 30000.0,1));
+        employees.add(new Employee(3, "xiaosan", 19, "Male", 30000.0,1));
+        employees.add(new Employee(4, "xiaosi", 19, "Male", 30000.0,1));
+        employees.add(new Employee(5, "xiaowu", 20, "Male", 30000.0,1));
+        employees.add(new Employee(6, "xiaoliu", 20, "Female", 30000.0,1));
+        employees.add(new Employee(7, "xiaoqi", 21, "Female", 30000.0,1));
+        employees.add(new Employee(8, "xiaoba", 21, "Female", 30000.0,1));
+        employees.add(new Employee(9, "xiaojiu", 18, "Male", 30000.0,1));
+        employees.add(new Employee(10, "xiaoshi", 18, "Male", 30000.0,1));
         List<Company> companies = new ArrayList<>();
         companies.add(new Company(1, "Mm", 10, employees));
         companies.add(new Company(2, "Aa", 10, employees));
@@ -161,31 +178,31 @@ public class CompanyIntegrationTest {
 
     private Company getMockCompany() {
         List<Employee> employees = new ArrayList<>();
-        employees.add(new Employee(1, "xiaoyi", 18, "Male",30000.0));
-        employees.add(new Employee(2, "xiaoer", 18, "Male", 30000.0));
-        employees.add(new Employee(3, "xiaosan", 19, "Male", 30000.0));
-        employees.add(new Employee(4, "xiaosi", 19, "Male", 30000.0));
-        employees.add(new Employee(5, "xiaowu", 20, "Male", 30000.0));
-        employees.add(new Employee(6, "xiaoliu", 20, "Female", 30000.0));
-        employees.add(new Employee(7, "xiaoqi", 21, "Female", 30000.0));
-        employees.add(new Employee(8, "xiaoba", 21, "Female", 30000.0));
-        employees.add(new Employee(9, "xiaojiu", 18, "Male", 30000.0));
-        employees.add(new Employee(10, "xiaoshi", 18, "Male", 30000.0));
+        employees.add(new Employee(1, "xiaoyi", 18, "Male", 30000.0,1));
+        employees.add(new Employee(2, "xiaoer", 18, "Male", 30000.0,1));
+        employees.add(new Employee(3, "xiaosan", 19, "Male", 30000.0,1));
+        employees.add(new Employee(4, "xiaosi", 19, "Male", 30000.0,1));
+        employees.add(new Employee(5, "xiaowu", 20, "Male", 30000.0,1));
+        employees.add(new Employee(6, "xiaoliu", 20, "Female", 30000.0,1));
+        employees.add(new Employee(7, "xiaoqi", 21, "Female", 30000.0,1));
+        employees.add(new Employee(8, "xiaoba", 21, "Female", 30000.0,1));
+        employees.add(new Employee(9, "xiaojiu", 18, "Male", 30000.0,1));
+        employees.add(new Employee(10, "xiaoshi", 18, "Male", 30000.0,1));
         return new Company(1, "Mm", 10, employees);
     }
 
-    private List<Employee> getMockEmployees(){
+    private List<Employee> getMockEmployees() {
         List<Employee> employees = new ArrayList<>();
-        employees.add(new Employee(1, "xiaoyi", 18, "Male", 30000.0));
-        employees.add(new Employee(2, "xiaoer", 18, "Male", 30000.0));
-        employees.add(new Employee(3, "xiaosan", 19, "Male", 30000.0));
-        employees.add(new Employee(4, "xiaosi", 19, "Male", 30000.0));
-        employees.add(new Employee(5, "xiaowu", 20, "Male", 30000.0));
-        employees.add(new Employee(6, "xiaoliu", 20, "Female",30000.0));
-        employees.add(new Employee(7, "xiaoqi", 21, "Female", 30000.0));
-        employees.add(new Employee(8, "xiaoba", 21, "Female", 30000.0));
-        employees.add(new Employee(9, "xiaojiu", 18, "Male", 30000.0));
-        employees.add(new Employee(10, "xiaoshi", 18, "Male", 30000.0));
+        employees.add(new Employee(1, "xiaoyi", 18, "Male", 30000.0,1));
+        employees.add(new Employee(2, "xiaoer", 18, "Male", 30000.0,1));
+        employees.add(new Employee(3, "xiaosan", 19, "Male", 30000.0,1));
+        employees.add(new Employee(4, "xiaosi", 19, "Male", 30000.0,1));
+        employees.add(new Employee(5, "xiaowu", 20, "Male", 30000.0,1));
+        employees.add(new Employee(6, "xiaoliu", 20, "Female", 30000.0,1));
+        employees.add(new Employee(7, "xiaoqi", 21, "Female", 30000.0,1));
+        employees.add(new Employee(8, "xiaoba", 21, "Female", 30000.0,1));
+        employees.add(new Employee(9, "xiaojiu", 18, "Male", 30000.0,1));
+        employees.add(new Employee(10, "xiaoshi", 18, "Male", 30000.0,1));
         return employees;
     }
 
