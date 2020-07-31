ea1c17bb86cbfa730fb2710208335fc9ce627dd8
Austin <497133212@qq.com>
2020-07-31 19:23:21 +0800

    [Company API][Austin Huang | Hardy] feature : add Data conversion type and mapper

---
+++ src/main/java/com/thoughtworks/springbootemployee/dto/EmployeeRequest.java
@@ -0,0 +1,85 @@
+package com.thoughtworks.springbootemployee.dto;
+
+import javax.persistence.Id;
+
+public class EmployeeRequest {
+    private Integer id;
+    private String name;
+    private int age;
+    private String gender;
+    private double salary;
+    private Integer companyId;
+
+    public EmployeeRequest() {
+    }
+
+    public EmployeeRequest(int id, String name, int age, String gender, double salary) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+        this.salary = salary;
+    }
+
+    public EmployeeRequest(int id, String name, int age, String gender, double salary, Integer companyId) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+        this.salary = salary;
+        this.companyId = companyId;
+    }
+
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    public Integer getCompanyId() {
+        return companyId;
+    }
+
+    public void setCompanyId(Integer companyId) {
+        this.companyId = companyId;
+    }
+}
